ebdb45edc361981095a6ffdcf88521185e0f6726
Shabbir Hasan <68828793+ShabbirHasan1@users.noreply.github.com>
2026-03-14 21:58:17 +0530

    Fix typo in JAINAM constant name
    Corrects a spelling mistake in the `JIANAM` static variable and its corresponding enum variant to `JAINAM`.

--- rama-http-headers/src/common/custom_allowed_ips.rs
+++ rama-http-headers/src/common/custom_allowed_ips.rs
@@ -1,7 +1,7 @@
 use serde_repr::{Deserialize_repr, Serialize_repr};
 use std::net::{IpAddr, Ipv4Addr};
 
-pub static JIANAM: IpAddr = IpAddr::V4(Ipv4Addr::new(103, 217, 67, 124));
+pub static JAINAM: IpAddr = IpAddr::V4(Ipv4Addr::new(103, 217, 67, 124));
 pub static SYMPHONYXTSDEVELOPER: IpAddr = IpAddr::V4(Ipv4Addr::new(160, 30, 125, 84));
 
 pub static ALLOWED_BROKER_IPS: [WhiteListedIps; 2] = WhiteListedIps::allowed_broker_ips();
@@ -11,14 +11,14 @@ pub static ALLOWED_BROKER_IPS: [WhiteListedIps; 2] = WhiteListedIps::allowed_bro
 )]
 #[repr(u8)]
 pub enum WhiteListedIps {
-    Jianam,
+    Jainam,
     SymphonyXtsDeveloper,
 }
 
 impl AsRef<IpAddr> for WhiteListedIps {
     fn as_ref(&self) -> &'static IpAddr {
         match self {
-            Self::Jianam => &JIANAM,
+            Self::Jainam => &JAINAM,
             Self::SymphonyXtsDeveloper => &SYMPHONYXTSDEVELOPER,
         }
     }
@@ -26,7 +26,7 @@ impl AsRef<IpAddr> for WhiteListedIps {
 
 impl WhiteListedIps {
     pub const fn allowed_broker_ips() -> [Self; 2] {
-        [Self::Jianam, Self::SymphonyXtsDeveloper]
+        [Self::Jainam, Self::SymphonyXtsDeveloper]
     }
 
     #[inline]
